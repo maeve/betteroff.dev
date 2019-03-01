f0afbbe04254fbd43dfd844ae9eb4d8605771a43
Maeve Revels <maeve.revels@getg5.com>
2019-03-01 14:43:34 -0800

    First pass

---
+++ dist/js/scripts.js
@@ -0,0 +1,10 @@
+(function() {
+  var blinks = document.getElementsByTagName('blink');
+  var visibility = 'hidden';
+  window.setInterval(function() {
+    for (var i = blinks.length - 1; i >= 0; i--) {
+      blinks[i].style.visibility = visibility;
+    }
+    visibility = (visibility === 'visible') ? 'hidden' : 'visible';
+  }, 250);
+})();

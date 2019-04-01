9dfceb687a062643b0d8343cf33e77a3dceb2b7f
Maeve Revels <maeve.revels@getg5.com>
2019-04-01 10:24:08 -0700

    prettier this way

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,10 +1,10 @@
 (function() {
-  var blinks = document.getElementsByTagName('blink');
-  var visibility = 'hidden';
+  var blinks = document.getElementsByTagName("blink");
+  var visibility = "hidden";
   window.setInterval(function() {
     for (var i = blinks.length - 1; i >= 0; i--) {
       blinks[i].style.visibility = visibility;
     }
-    visibility = (visibility === 'visible') ? 'hidden' : 'visible';
+    visibility = visibility === "visible" ? "hidden" : "visible";
   }, 250);
 })();
